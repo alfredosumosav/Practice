1d6e7c356497db2b14a6c38417670fafc4277ebe
alfredosumosav <asumosa1993@gmail.com>
2020-11-10 18:58:13 -0800

    add solution without converting to string

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -1,3 +1,4 @@
+// O(n) time || O(n) space
 var isPalindrome = function (x) {
   if (x < 0) return false;
 
@@ -10,6 +11,40 @@ var isPalindrome = function (x) {
   }
 
   return x2 === res;
+}
+
+// Without converting to string
+var isPalindrome = function (x) {
+  // copy input
+  let n = x;
+  if (n < 0) return false;
+
+  // store decomposed numbers
+  let array = [];
+
+  // perform decomposition
+  while (n !== 0) {
+    let rem = n % 10;
+    array.unshift(rem);
+    n = Math.floor(n / 10);
+  }
+
+  let first = 0; // first pointer
+  let last = array.length - 1; // last pointer
+  let isNumPalindrome = true; // result
+  
+  // use two pointers technique to check palindromicity
+  while (first <= last) {
+    if (array[first] !== array[last]) {
+      // is not a palindrome
+      isNumPalindrome = false;
+      break;
+    }
+    first++;
+    last--;
+  }
+  // it is palindrome
+  return isNumPalindrome;
 }
 
 console.log(isPalindrome(121)); // true
