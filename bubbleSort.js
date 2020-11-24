3bfac4376547a46e0651026ea8cc7b57008b7475
alfredosumosav <asumosa1993@gmail.com>
2020-11-24 14:42:40 -0800

    Add desc condition

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,17 +1,26 @@
+// Swap 2 elements
+function swap(array, idx1, idx2) {
+  const temp = array[idx1];
+  array[idx1] = array[idx2];
+  array[idx2] = temp;
+  return array;
+}
+
 function bubbleSort(array) {
   let swapped = true;
 
-  // Keep scanning through the array until there is no need to swap (is ordered)
+  // Keep scanning the array until there is no need to swap (is ordered)
   while (swapped) {
     swapped = false;
 
-    // pass through every element and if current element and next element
-    // are unordered, fix the order by swapping them
+    // pass through every element.
     for (let i = 0; i < array.length - 1; i++) {
-      // if current element is greater than the next element, the array is
-      // unordered, so swap it
-      if (array[i] > array[i + 1]) {
-        swap(array, i, i + 1); // swap current element with next element
+      const unordered = array[i] > array[i + 1]; // ASC order
+      // const unordered = array[i] < array[i + 1]; // DSC order
+
+      // if currentElement > nextElement, it's unordered (ASC), so swap them.
+      if (unordered) {
+        swap(array, i, i + 1);
         swapped = true; // remember that you swapped
       }
     }
@@ -19,3 +28,5 @@ function bubbleSort(array) {
 
   return array;
 }
+
+console.log(bubbleSort([9, 7, 8, 8, 9, 7]));
