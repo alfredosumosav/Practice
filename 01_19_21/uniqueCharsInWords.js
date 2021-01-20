1dd8ebd0f3122603c223fb9f6cca47893588ccfc
alfredosumosav <asumosa1993@gmail.com>
2021-01-19 17:52:43 -0800

    improve time and space complexity

--- 01_19_21/uniqueCharsInWords.js
+++ 01_19_21/uniqueCharsInWords.js
@@ -7,40 +7,6 @@ otherwise. The same character can appear across different words, but within a
 single word no character should appear more than once.
 */
 
-// Time and Space Complexity Analysis
-// O(n * m) time and O(n) space, where n is the number of words in a sentence
-// and m is the max number of character in a word
-// function uniqueCharsInWords(sentence) {
-//   var words = sentence.split(" ");
-//   for (var i = 0; i < words.length; i++) {
-//     var word = words[i];
-//     if (!unique(word)) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
-// function unique(str) {
-//   var seenChars = [];
-
-//   for (var i = 0; i < str.length; i++) {
-//     var char = str[i];
-//     if (seenChars.indexOf(char) === -1) {
-//       seenChars.push(char);
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// }
-
-// console.log(uniqueCharsInWords('this has repeated chars')); // => false
-// console.log(uniqueCharsInWords('this has no duplicate chars')); // => true
-// console.log(uniqueCharsInWords('javascript is fun')); // => false
-// console.log(uniqueCharsInWords('coding is fun')); // => true
-
 var uniqueCharsinWord = function (sentence) {
   let alpha = "abcdefghijklmnopqrstvwxyz".split("");
   let alphaHash = {};
