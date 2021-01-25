5861e3650e96eceeaa91430884b362767e745191
alfredosumosav <asumosa1993@gmail.com>
2021-01-24 16:44:23 -0800

    doubleLetterCount

---
+++ 01_24_21/doubleLetterCount.js
@@ -0,0 +1,42 @@
+/*
+Write a method that takes in a string and returns
+the number of times that the same letter repeats twice in a row.
+*/
+
+function doubleLetterCount(str) {
+  var count = 0;
+  var arr = str.split(" ");
+
+  for (var i = 0; i < arr.length; i++) {
+    var word = arr[i];
+    count += doubleCounter(word);
+  }
+
+  return count;
+}
+
+function doubleCounter(word) {
+  var count = 0;
+  var bool = true;
+
+  for (var i = 1; i < word.length; i++) {
+    var current = word[i];
+    var prev = word[i - 1];
+    var next = word[i + 1];
+    if (current === prev && current === next) {
+      bool = false;
+      continue;
+    } else if (current === prev && current !== next && bool === false) {
+      continue;
+    } else if (current === prev && current !== next && bool === true) {
+      count += 1;
+    }
+    bool = true;
+  }
+
+  return count;
+}
+
+console.log(doubleCounter("jeep")); // => 1
+console.log(doubleLetterCount("the jeep rolled down the hill")); // 3
+console.log(doubleLetterCount("bootcamp")); // 1
