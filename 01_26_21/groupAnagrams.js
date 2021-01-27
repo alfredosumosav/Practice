36a6f409a31a27d05b23ad30682e7ba6f3ea72dc
alfredosumosav <asumosa1993@gmail.com>
2021-01-26 17:52:04 -0800

    Add test cases

--- 01_26_21/groupAnagrams.js
+++ 01_26_21/groupAnagrams.js
@@ -14,3 +14,18 @@ function groupAnagrams(words) {
 
   return Object.values(anagrams);
 }
+
+console.log(groupAnagrams(["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"])); // [["yo", "oy"], ["act", "tac", "cat"], ["flop", "olfp"], ["foo"]]
+console.log(groupAnagrams([])); // []
+console.log(groupAnagrams(["test"])); // [["test"]]
+console.log(groupAnagrams(["abc", "dabd", "bca", "cab", "ddba"])); // [["abc", "bca", "cab"], ["dabd", "ddba"]]
+console.log(groupAnagrams(["abc", "cba", "bca"])); // [["abc", "cba", "bca"]]
+console.log(groupAnagrams(["zxc", "asd", "weq", "sda", "qwe", "xcz"])); // [["zxc", "xcz"], ["asd", "sda"], ["weq", "qwe"]]
+console.log(groupAnagrams(["cinema", "a", "flop", "iceman", "meacyne", "lofp", "olfp"])); // [["cinema", "iceman"], ["a"], ["flop", "lofp", "olfp"], ["meacyne"]]
+console.log(groupAnagrams(["abc", "abe", "abf", "abg"])); // [["abc"], ["abe"], ["abf"], ["abg"]]
+console.log(groupAnagrams(["aaa", "a"])); // [["aaa"], ["a"]]
+console.log(groupAnagrams(["bob", "boo"])); // [["bob"], ["boo"]]
+console.log(groupAnagrams(["ill", "duh"])); // [["ill"], ["duh"]]
+console.log(groupAnagrams(["yo", "oy", "zn"])); // [["yo", "oy"], ["zn"]]
+console.log(groupAnagrams(["yyo", "yo"])); // [["yyo"], ["yo"]]
+console.log(groupAnagrams(["aca", "bba"])); // [["aca"], ["bba"]]
