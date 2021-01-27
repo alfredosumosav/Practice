997c88ddbfe14246538b6263932b4c0e1d232637
alfredosumosav <asumosa1993@gmail.com>
2021-01-26 17:57:27 -0800

    Add test cases

--- 01_26_21/longestPalindromicSum.js
+++ 01_26_21/longestPalindromicSum.js
@@ -26,3 +26,18 @@ function isPalindrome(string) {
   let reversed = string.split("").reverse().join("");
   return string === reversed;
 }
+
+console.log(longestPalindromicSubstring("abaxyzzyxf")); //"xyzzyx"
+console.log(longestPalindromicSubstring("a")); // "a"
+console.log(longestPalindromicSubstring("it's highnoon")); // "noon"
+console.log(longestPalindromicSubstring("noon high it is")); // "noon"
+console.log(longestPalindromicSubstring("abccbait's highnoon")); // "abccba"
+console.log(longestPalindromicSubstring("abcdefgfedcbazzzzzzzzzzzzzzzzzzzz")); // "zzzzzzzzzzzzzzzzzzzz"
+console.log(longestPalindromicSubstring("abcdefgfedcba")); // "abcdefgfedcba"
+console.log(longestPalindromicSubstring("abcdefghfedcbaa")); // "aa"
+console.log(longestPalindromicSubstring("abcdefggfedcba")); // "abcdefggfedcba"
+console.log(longestPalindromicSubstring("zzzzzzz2345abbbba5432zzbbababa")); // "zz2345abbbba5432zz"
+console.log(longestPalindromicSubstring("z234a5abbbba54a32z")); // "5abbbba5"
+console.log(longestPalindromicSubstring("z234a5abbba54a32z")); // "5abbba5"
+console.log(longestPalindromicSubstring("ab12365456321bb")); // "b12365456321b"
+console.log(longestPalindromicSubstring("aca")); // "aca"
