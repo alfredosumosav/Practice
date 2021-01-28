08f987cbd93702504ec9e403f60e08fa3e210246
alfredosumosav <asumosa1993@gmail.com>
2021-01-27 17:19:10 -0800

    mergeIntervals

---
+++ mergeIntervals.js
@@ -0,0 +1,27 @@
+var merge = function (intervals) {
+  // sort intervals in order (start_i as reference)
+  intervals = intervals.sort((a, b) => a[0] - b[0]);
+  let nonOverlappingIntervals = [intervals.shift()];
+
+  // loop through intervals, until there is no more overlapping elements
+  while (intervals.length) {
+    // check contiguos intervals for overlaping
+    let i1 = nonOverlappingIntervals.pop();
+    let i2 = intervals.shift();
+
+    // if they overlap, merge them and save into result
+    if (i1[1] - i2[0] >= 0) {
+      let smallestStart = Math.min(i1[0], i2[0]);
+      let largestEnd = Math.max(i1[1], i2[1]);
+      let merged = [smallestStart, largestEnd];
+      nonOverlappingIntervals.push(merged);
+    } else {
+      // otherwise, they do not overlap, so push the intervals to the result
+      // save them in a non-overlapping array (result)
+      nonOverlappingIntervals.push(i1);
+      nonOverlappingIntervals.push(i2);
+    }
+  }
+  // return the non-overlapping interval
+  return nonOverlappingIntervals;
+};
