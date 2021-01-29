cf1a14573b7ab4e4d2818aee8844463e70415a08
alfredosumosav <asumosa1993@gmail.com>
2021-01-28 17:45:43 -0800

    add time and space complexity

--- 01_28_21/vigenereCipher.js
+++ 01_28_21/vigenereCipher.js
@@ -21,6 +21,7 @@
 // var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 ------------------------------------------------------------------------------*/
 
+// O(n) time and O(n) space, where n is the length of the input string
 function vigenereCipher(string, keySequence, alphabet) {
   var ans = "";
 
