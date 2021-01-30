f103954ef32c7d31a9232c15499b5c87dfc666d5
alfredosumosav <asumosa1993@gmail.com>
2021-01-29 16:19:55 -0800

    Add description

--- 01_29_21/uniqueItems.js
+++ 01_29_21/uniqueItems.js
@@ -1,3 +1,17 @@
+/*
+write a function that takes in an array. It should return an new array containing
+only the elements that were unique in the original array.
+
+Note that any items which were duplicated in the original array will not appear 
+**at all** in the result
+
+Examples:
+
+uniqueItems([1 , 1]); -> []
+uniqueItems([1, 2, 3]); -> [1, 2, 3]
+uniqueItems([1, 2, 2]); -> [1]
+uniqueItems([1, 5, 5, 7, 16, 8, 1, 8]); -> [7, 16]
+*/
 // O(n) time and O(n) space, where n is the number of elements in the input array
 function uniqueItems(array) {
   var obj = {};
