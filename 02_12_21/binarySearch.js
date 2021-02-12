d58f46061b5ea224babbeab9863a76971e8af769
alfredosumosav <asumosa1993@gmail.com>
2021-02-12 14:19:30 -0800

    binarySearch

---
+++ 02_12_21/binarySearch.js
@@ -0,0 +1,20 @@
+// https://www.algoexpert.io/questions/Binary%20Search
+
+function binarySearch(array, target) {
+  let left = 0;
+  let right = array.length - 1;
+  let mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (target === array[mid]) {
+      return mid;
+    } else if (target < array[mid]) {
+      right = mid - 1;
+    } else if (target > array[mid]) {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+}
