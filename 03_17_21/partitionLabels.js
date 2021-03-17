4cf857793f10c11aac8bc45a3740801b135adf6b
alfredosumosav <asumosa1993@gmail.com>
2021-03-17 16:29:11 -0700

    partitionLabels

---
+++ 03_17_21/partitionLabels.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/partition-labels/
+
+var partitionLabels = function (S) {
+  const dict = "abcdefghijklmnopqrstuvwxyz";
+  let lastIndexes = {};
+
+  for (const letter of dict) {
+    let lastIndex = S.lastIndexOf(letter);
+    if (lastIndex > -1) {
+      lastIndexes[letter] = lastIndex;
+    }
+  }
+
+  let left = 0;
+  let right = 0;
+  let res = [];
+
+  for (let i = 0; i < S.length; i++) {
+    let letter = S[i];
+
+    if (lastIndexes[letter] > right) {
+      right = lastIndexes[letter];
+    } else if (i === right) {
+      res.push(lastIndexes[letter] - left + 1);
+      left = i + 1;
+      right = lastIndexes[letter] + 1;
+    }
+  }
+
+  return res;
+};
