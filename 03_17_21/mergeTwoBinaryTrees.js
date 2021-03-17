8e3878d6753ad0f7b9a58d9d9d363968970e1b8b
alfredosumosav <asumosa1993@gmail.com>
2021-03-17 16:54:27 -0700

    mergeTwoBinaryTrees

---
+++ 03_17_21/mergeTwoBinaryTrees.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/merge-two-binary-trees/
+
+var mergeTrees = function (root1, root2) {
+  // if one of the nodes is null, return the other
+  if (!root1 || !root2) {
+    return root1 || root2;
+  }
+
+  // if both nodes exist, add their values
+  root1.val += root2.val;
+
+  // recursively merge left and right subtree of both nodes
+  root1.left = mergeTrees(root1.left, root2.left);
+  root1.right = mergeTrees(root1.right, root2.right);
+
+  // return the merged node
+  return root1;
+};
