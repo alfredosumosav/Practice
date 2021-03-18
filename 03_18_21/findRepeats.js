6a5664dba5db43ddb6d1ae466a5fac413ea46a22
alfredosumosav <asumosa1993@gmail.com>
2021-03-18 16:40:27 -0700

    add problem description

--- 03_18_21/findRepeats.js
+++ 03_18_21/findRepeats.js
@@ -1,4 +1,19 @@
-// https://www.interviewcake.com/question/javascript/find-duplicate-optimize-for-space
+/*
+We have an array of integers, where:
+
+  1. The integers are in the range 1...n
+  2. The array has a length of n+1
+  
+It follows that our array has at least one integer which appears at least twice. 
+But it may have several duplicates, and each duplicate may appear more than twice.
+
+Write a function which finds an integer that appears more than once in our array. 
+Don't modify the input! (If there are multiple duplicates, you only need to find one of them.)
+
+We're going to run this function on our new, super-hip MacBook Pro With Retina Display™. 
+Thing is, the damn thing came with the RAM soldered right to the motherboard, so 
+we can't upgrade our RAM.So we need to optimize for space!
+*/
 
 function findRepeat(numbers) {
 
