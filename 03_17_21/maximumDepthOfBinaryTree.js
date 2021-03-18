cdd7b39873ea3d769836a76599ff2dc6aa034f86
alfredosumosav <asumosa1993@gmail.com>
2021-03-17 17:48:09 -0700

    add comments

--- 03_17_21/maximumDepthOfBinaryTree.js
+++ 03_17_21/maximumDepthOfBinaryTree.js
@@ -1,13 +1,17 @@
 // https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
 var maxDepth = function (root, initialDepth = 0) {
+  // if the node is null, return the initial depth
   if (!root) return initialDepth;
 
+  // increase the depth by 1, to include current level
   initialDepth++;
 
+  // recursively find the max depth of each subtree
   let leftDepth = root.left ? maxDepth(root.left, initialDepth) : 0;
   let rightDepth = root.right ? maxDepth(root.right, initialDepth) : 0;
 
+  // return the deepest path
   initialDepth = Math.max(initialDepth, leftDepth, rightDepth);
 
   return initialDepth;
