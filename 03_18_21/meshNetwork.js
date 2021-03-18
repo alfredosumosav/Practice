b4256f7ef13b4a30de2ed2c54902e86c45c6368c
alfredosumosav <asumosa1993@gmail.com>
2021-03-18 15:03:17 -0700

    store additional information to reconstruct path

--- 03_18_21/meshNetwork.js
+++ 03_18_21/meshNetwork.js
@@ -1,7 +1,7 @@
 // Assume we have an efficient queue implementation, Queue()
 // with enqueue and dequeue methods and a size property
 
-function bfs(graph, startNode, endNode) {
+function bfsGetPath(graph, startNode, endNode) {
   const nodesToVisit = new Queue();
   nodesToVisit.enqueue(startNode);
 
@@ -9,19 +9,27 @@ function bfs(graph, startNode, endNode) {
   // so we don't process them twice
   const nodesAlreadySeen = new Set([startNode]);
 
+  // Keep track of how we got to each node
+  // we'll use this to reconstruct the shortest path at the end
+  const howWeReachedNodes = {};
+  howWeReachedNodes[startNode] = null;
+
   while (nodesToVisit.size > 0) {
     const currentNode = nodesToVisit.dequeue();
 
     // Stop when we reach the end node
     if (currentNode === endNode) {
-      // Found it!
-      break;
+      // Somehow reconstruct the path here
+      return path;
     }
 
     graph[currentNode].forEach((neighbor) => {
       if (!nodesAlreadySeen.has(neighbor)) {
         nodesAlreadySeen.add(neighbor);
         nodesToVisit.enqueue(neighbor);
+
+        // Keep track of how we got to this node
+        howWeReachedNodes[neighbor] = currentNode;
       }
     });
   }
