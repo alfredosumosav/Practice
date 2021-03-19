0a028e275045713a16a3886051b7005d151eaec3
alfredosumosav <asumosa1993@gmail.com>
2021-03-19 16:55:19 -0700

    add description

--- 03_19_21/hasPalindromePermutation.js
+++ 03_19_21/hasPalindromePermutation.js
@@ -1,4 +1,21 @@
-  function hasPalindromePermutation(theString) {
+/*
+Write an efficient function that checks whether any permutation ↴ of an input 
+string is a palindrome.
+
+You can assume the input string only contains lowercase letters.
+
+Examples:
+
+  "civic" should return true
+  "ivicc" should return true
+  "civil" should return false
+  "livci" should return false
+*/
+
+// Time and space complexity
+// O(n) time, where n is the the size of the string input
+// O(k) space, where k is the number of unique characters (ASCII: 128, Unicode: 100.000)
+function hasPalindromePermutation(theString) {
 
   // Track characters we've seen an odd number of times
   const unpairedCharacters = new Set();
