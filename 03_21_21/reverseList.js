ee174643f0952d6b32434cdbb2c7de5650fb21e5
alfredosumosav <asumosa1993@gmail.com>
2021-03-21 17:13:13 -0700

    reverseList

---
+++ 03_21_21/reverseList.js
@@ -0,0 +1,17 @@
+var reverseList = function (head) {
+  let currentNode = head;
+  let previousNode = null;
+  let nextNode = null;
+
+  while (currentNode) {
+    nextNode = currentNode.next;
+
+    currentNode.next = previousNode;
+
+    previousNode = currentNode;
+
+    currentNode = nextNode;
+  }
+
+  return previousNode;
+};
