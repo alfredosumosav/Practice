619517bc1b4e9ac710b59f865353f81c7989996d
alfredosumosav <asumosa1993@gmail.com>
2021-03-21 17:21:49 -0700

    document thought process

--- 03_21_21/reverseList.js
+++ 03_21_21/reverseList.js
@@ -7,13 +7,18 @@ var reverseList = function (head) {
   let previousNode = null;
   let nextNode = null;
 
+  // Until we have 'fallen off' the end of the list
   while (currentNode) {
+
+    // Copy a pointer to the next element
+    // before we overwrite currentNode.next
     nextNode = currentNode.next;
 
+    // Reverse the 'next' pointer
     currentNode.next = previousNode;
 
+    // Step forward in the list
     previousNode = currentNode;
-
     currentNode = nextNode;
   }
 
