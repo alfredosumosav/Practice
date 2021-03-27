0d8f35028cd051c0d0781be50b0ecdde60b593dd
alfredosumosav <asumosa1993@gmail.com>
2021-03-27 00:18:39 -0700

    getPermutations

--- 03_26_21/getPermutations.js
+++ 03_26_21/getPermutations.js
@@ -5,7 +5,22 @@ function getPermutations(string) {
     return new Set([string]);
   }
 
+  const allCharsExceptTheLast = string.slice(0, -1);
+  const lastChar = string[string.length - 1];
+
   // Recursive case: get all permutations for all the characters, except the last
+  const permutationsOfAllCharsExceptLast = getPermutations(allCharsExceptTheLast);
+
+  // put the last char in all possible positions for each of the above permutations
+  let permutations = new Set();
+  permutationsOfAllCharsExceptLast.forEach(permutationOfAllCharsExceptLast => {
+    for (let position = 0; position <= allCharsExceptTheLast.length; position++) {
+      let permutation = permutationOfAllCharsExceptLast.slice(0, position) + lastChar + permutationOfAllCharsExceptLast.slice(position);
+      permutations.add(permutation);
+    }
+  });
+
+  return permutations;
 }
 
 console.log(getPermutations('cats'));
