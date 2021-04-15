3eea8527678accc027302dd097c0063ca4a69840
alfredosumosav <asumosa1993@gmail.com>
2021-04-15 16:00:50 -0700

    add description

--- 04_15_21/arraySign.js
+++ 04_15_21/arraySign.js
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/sign-of-the-product-of-an-array
+
+// O(n) time and O(n) space
 var arraySign = function (nums) {
   const nums2 = nums.map(num => num < 0 ? -1 : num > 0 ? 1 : 0);
   if (nums2.some(num => num === 0)) return 0;
