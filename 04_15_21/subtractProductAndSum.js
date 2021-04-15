201213d778f1dd73294f7c00e73a9fd387fdc646
alfredosumosav <asumosa1993@gmail.com>
2021-04-15 16:36:22 -0700

    add description and complexity

--- 04_15_21/subtractProductAndSum.js
+++ 04_15_21/subtractProductAndSum.js
@@ -1,4 +1,7 @@
-var subtractProductAndSum = function(n) {
+// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer
+
+// O(d) time and O(1) space, where d is the number of digits from the input number
+var subtractProductAndSum = function (n) {
     
     let prod = 1;
     let sum = 0;
