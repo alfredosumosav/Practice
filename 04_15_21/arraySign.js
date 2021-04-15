4862b20ac104c7faf7e867dd847a0317ec1fa9fa
alfredosumosav <asumosa1993@gmail.com>
2021-04-15 16:07:04 -0700

    improve time complexity

--- 04_15_21/arraySign.js
+++ 04_15_21/arraySign.js
@@ -1,8 +1,8 @@
 // https://leetcode.com/problems/sign-of-the-product-of-an-array
 
-// O(n) time and O(n) space
+// O(n) time and O(1) space
 var arraySign = function (nums) {
-  const nums2 = nums.map(num => num < 0 ? -1 : num > 0 ? 1 : 0);
-  if (nums2.some(num => num === 0)) return 0;
-  return nums2.reduce((acc, num) => acc * num)
+  nums.forEach((num, i) => num < 0 ? nums[i] = -1 : num > 0 ? nums[i] = 1 : nums[i] = 0);
+  if (nums.some(num => num === 0)) return 0;
+  return nums.reduce((acc, num) => acc * num);
 };
