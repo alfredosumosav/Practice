7d6b68810652c926f080e720c338daa2710348a0
alfredosumosav <asumosa1993@gmail.com>
2021-04-21 16:55:32 -0700

    Add test cases and problem description

--- 04_20_21/getClosingParen.js
+++ 04_20_21/getClosingParen.js
@@ -1,3 +1,17 @@
+/*
+I like parentheticals (a lot).
+
+"Sometimes (when I nest them (my parentheticals) too 
+much (like this (and this))) they get confusing."
+
+Write a function that, given a sentence like the one above, along with the 
+position of an opening parenthesis, finds the corresponding closing parenthesis.
+
+Example: if the example string above is input with the 
+number 10 (position of the first parenthesis), the output 
+should be 79 (position of the last parenthesis).
+*/
+
 function getClosingParen(sentence, openingParenIndex) {
   let openNestedParens = 0;
 
@@ -16,3 +30,7 @@ function getClosingParen(sentence, openingParenIndex) {
 
   throw new Error('No closing parenthesis :(');
 }
+
+console.log(getClosingParen('((((()))))', 2)); // 7
+console.log(getClosingParen('()()((()()))', 5)); // 10
+console.log(getClosingParen('()(()', 2)); // No matching closer
