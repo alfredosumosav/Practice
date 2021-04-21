ce549889280992aa015e252774686de20254a1ab
alfredosumosav <asumosa1993@gmail.com>
2021-04-21 16:50:56 -0700

    getClosingParen

---
+++ 04_20_21/getClosingParen.js
@@ -0,0 +1,18 @@
+function getClosingParen(sentence, openingParenIndex) {
+  let openNestedParens = 0;
+
+  for (let position = openingParenIndex + 1; position < sentence.length; position++) {
+    const char = sentence[position];
+
+    if (char === '(') {
+      openNestedParens += 1;
+    } else if (char === ')') {
+      if (openNestedParens === 0) {
+        return position;
+      }
+      openNestedParens -= 1;
+    }
+  }
+
+  throw new Error('No closing parenthesis :(');
+}
