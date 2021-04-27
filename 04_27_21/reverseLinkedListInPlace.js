20fc621829a5bb5822b59aaa8dfe4200365d3d57
alfredosumosav <asumosa1993@gmail.com>
2021-04-27 15:10:05 -0700

    Initial commit

---
+++ 04_27_21/reverseLinkedListInPlace.js
@@ -0,0 +1,12 @@
+class LinkedListNodes {
+  constructor(value = null) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function reverse(headOfList) {
+  return null;
+}
+
+console.log(reverse(node));
