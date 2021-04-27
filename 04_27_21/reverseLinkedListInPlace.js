07e28e620e1c3ea5214cdf91ca935c6939582293
alfredosumosav <asumosa1993@gmail.com>
2021-04-27 15:19:13 -0700

    create a function to generate a list of linked list of nodes from an array of values

--- 04_27_21/reverseLinkedListInPlace.js
+++ 04_27_21/reverseLinkedListInPlace.js
@@ -5,6 +5,33 @@ class LinkedListNodes {
   }
 }
 
+// creates a linked list from the array of input values
+function valuesToLinkedListNodes(values) {
+
+  // store the nodes created
+  const nodes = [];
+
+  // for every value in the input array, create a new node and link it
+  // to the previous node if at least there are more than one values
+  for (let i = 0; i < values; i++) {
+
+    // create a new node with the current value
+    const node = new LinkedListNodes(values[i]);
+
+    // if I have a previous node...
+    if (i > 0) {
+      // point the next property to the current node
+      nodes[i - 1].next = node;
+    }
+
+    // add current node to the nodes store
+    nodes.push(node);
+  }
+
+  // return the linked list of nodes
+  return nodes;
+}
+
 function reverse(headOfList) {
   return null;
 }
