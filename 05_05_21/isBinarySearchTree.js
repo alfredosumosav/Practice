804310cab544e8405a0cc4f97cff085342f76143
alfredosumosav <asumosa1993@gmail.com>
2021-05-05 15:41:13 -0700

    isBinarySearchTree

---
+++ 05_05_21/isBinarySearchTree.js
@@ -0,0 +1,31 @@
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left  = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+function isBinarySearchTree(treeRoot, lowerBound, upperBound) {
+  lowerBound = (typeof lowerBound !== 'undefined') ? lowerBound : Number.NEGATIVE_INFINITY;
+  upperBound = (typeof upperBound !== 'undefined') ? upperBound : Number.POSITIVE_INFINITY;
+
+  if (!treeRoot) return true;
+
+  if (treeRoot.value >= upperBound || treeRoot.value <= lowerBound) {
+    return false;
+  }
+
+  return isBinarySearchTree(treeRoot.left, lowerBound, treeRoot.value)
+    && isBinarySearchTree(treeRoot.right, treeRoot.value, upperBound);
+}
