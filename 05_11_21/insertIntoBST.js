20ac40515bce838bd39b5ba1d0df48e094fe0315
alfredosumosav <asumosa1993@gmail.com>
2021-05-11 18:33:00 -0700

    add description, documentation and complexity analysis

--- 05_11_21/insertIntoBST.js
+++ 05_11_21/insertIntoBST.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
 class TreeNode {
   constructor(val = 0, left = null, right = null) {
     this.val = val;
@@ -6,23 +8,40 @@ class TreeNode {
   }
 }
 
+// Time and Space complexity analysis
+// O(h) time and O(h) space, where h is the height of the BST
 var insertIntoBST = function (root, val) {
 
+  // if root is empty, set the root with the value provided
   if (!root) {
     root = new TreeNode(val);
     return root;
   }
 
+  // if the value is smaller than the root's value...
   if (val < root.val) {
-    if (!root.left) {
+
+    if (!root.left) {  // ...and there is no left child
+
+      // set the left child of the root with the value
       root.left = new TreeNode(val);
-    } else {
+    } else {  // ...and the left child already exists
+
+      // recursively insert the value on the left subtree of the root
       insertIntoBST(root.left, val);
     }
+
+    // otherwise, the value is greater than or equal than the root...
   } else {
-    if (!root.right) {
+
+
+    if (!root.right) {  // ...and there is no right child
+
+      // set the right child of the root with the value
       root.right = new TreeNode(val);
-    } else {
+    } else {  // ...and the right child already exists
+
+      // recursively insert the value on the right subtree of the root
       insertIntoBST(root.right, val);
     }
   }
