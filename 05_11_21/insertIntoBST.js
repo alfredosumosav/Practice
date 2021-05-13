2aa8b6593fa5e1d01131a4a1ebd1acb265e3a3af
alfredosumosav <asumosa1993@gmail.com>
2021-05-12 23:53:22 -0700

    add tests

--- 05_11_21/insertIntoBST.js
+++ 05_11_21/insertIntoBST.js
@@ -49,18 +49,18 @@ var insertIntoBST = function (root, val) {
   return root;
 }
 
-// const root = new TreeNode(4);
-// const n1 = new TreeNode(1);
-// const n2 = new TreeNode(2);
-// const n3 = new TreeNode(3);
-// const n4 = new TreeNode(7);
+const root = new TreeNode(4);
+const n1 = new TreeNode(1);
+const n2 = new TreeNode(2);
+const n3 = new TreeNode(3);
+const n4 = new TreeNode(7);
 
-// root.left = n2;
-// root.right = n4;
-// n2.left = n1;
-// n2.right = n3;
+root.left = n2;
+root.right = n4;
+n2.left = n1;
+n2.right = n3;
 
-// console.log(insertIntoBST(root, 5)); //
+console.log(insertIntoBST(root, 5)); //
 
 module.exports = {
   insertIntoBST,
